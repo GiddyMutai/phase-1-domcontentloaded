0bcb0731ea85bd58e12775229f694ae7b7388323
Gideon Mutai <giddymutai13@gmail.com>
2024-04-10 16:38:29 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+// Event listener to check if DOM has loaded
+document.addEventListener('DOMContentLoaded', () =>{
+    let par = document.getElementById('text')
+    par.textContent = ('This is really cool!')
+})
